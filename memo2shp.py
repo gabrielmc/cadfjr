c88faa4c24ccf0aa9082bfe6a3a518b92ab7883e
Gabriel Muniz <munhosga@gmail.com>
2021-09-23 17:02:38 -0300

    Mudanças sutis
    Mudança de nomenclatura para deixar o código mais claro

--- memo2shp.py
+++ memo2shp.py
@@ -1,43 +1,38 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Aug 31 22:49:24 2021
-
 @author: cadfj
 """
-
 
 import re
 import pandas as pd
 import geopandas as gpd
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot
 from shapely.geometry import point
 
 
-
-
-# Lembrar com relação ao banco não serão para o usuário passar como parametro.
 string = input('Copie e cole o texto do seu memorial descritivo aqui:')
 EPSG = int(input('EPSG de saída: '))
 caminho_saida = input('insira o caminho de saída mais o nome do arquivo com a extensão (ex: C:\geo\shape_python\saida1.shp): ')
-substituicao = re.sub(r'(?<=\d)[.]','', string)
-substituicao2 = re.sub(r'(?<=\d)[,]','.', substituicao)
-x = (re.findall(r" \d{6}\ | \d{6}\.\d{2,3}", substituicao2))
-y = (re.findall(r" \d{7}\ | \d{7}.\d{2,3}", substituicao2))
+texto = re.sub(r'(?<=\d)[.]','', string)
+texto_decodificado = re.sub(r'(?<=\d)[,]','.', texto)
 
-d = {'col1':x,'col2':y}
-df = pd.DataFrame(d)
-gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.col1, df.col2), crs= EPSG)
 
+ColunaX = (re.findall(r" \d{6}\ | \d{6}\.\d{2,3}", texto_decodificado))
+ColunaY = (re.findall(r" \d{7}\ | \d{7}.\d{2,3}", texto_decodificado))
+
+tupla = {'col1':ColunaX,'col2':ColunaY}
+df = pd.DataFrame(tupla)
+geometry = gpd.points_from_xy(df.col1, df.col2)
+gdf = gpd.GeoDataFrame(df, geometry=geometry, crs= EPSG)
 
 df.to_excel(r'C:\geo\csv\teste5.xlsx')
 gdf.to_file(caminho_saida)
 
-print(substituicao2)
+#print(texto_decodificado)
 print(df)
 
 gdf.plot(figsize=(16,14), facecolor='white', edgecolor='black') #ax=ax
-
-plt.title('Coordenadas Memorial')
-plt.show()
-
+plot.title('Coordenadas Memorial')
+plot.show()
 
